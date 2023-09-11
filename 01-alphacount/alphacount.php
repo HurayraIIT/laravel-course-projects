9f512d5cc492c9bd2607a18cc05c540e7d30a43e
Abu Hurayra <hurayraiit@gmail.com>
2023-09-11 23:57:25 +0600

    completed: 01-alphacount

--- 01-alphacount/alphacount.php
+++ 01-alphacount/alphacount.php
@@ -1,3 +1,33 @@
+#!/opt/homebrew/bin/php
 <?php
 
-echo "Hello World!";
+// Handle command line arguments
+// In this case only help (-h/--h) will be handled.
+$options = getopt($short_options="h", $long_options=["h"]);
+
+function run_helper(array $options)
+{
+  if (isset($options['h'])) {
+    echo "This program receives a string and outputs the alphabet count.\nGood luck!\n";
+    exit;
+  }
+}
+
+function find_alpha_count(string $input) : int {
+  $numOfChars = 0;
+  $chars = str_split($input);
+  foreach ($chars as $char) {
+    if (ctype_alpha($char)) {
+      $numOfChars += 1;
+    }
+  }
+  return $numOfChars;
+}
+
+// The main app loop
+while (true) {
+  run_helper($options);
+
+  $input = readline("\nEnter a string (ctrl-c to quit):");
+  echo find_alpha_count($input);
+}
